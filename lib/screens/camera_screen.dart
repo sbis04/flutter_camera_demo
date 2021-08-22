ab45dcdb05a643b516cba3aac72dc1c7a61ede45
Souvik Biswas <sbis1999@gmail.com>
2021-08-22 19:27:31 +0530

    Add zoom level

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -12,7 +12,16 @@ class _CameraScreenState extends State<CameraScreen>
     with WidgetsBindingObserver {
   CameraController? controller;
   bool _isCameraInitialized = false;
+  double _minAvailableExposureOffset = 0.0;
+  double _maxAvailableExposureOffset = 0.0;
+  double _minAvailableZoom = 1.0;
+  double _maxAvailableZoom = 1.0;
+
+  double _currentZoomLevel = 1.0;
+  double _currentExposureOffset = 0.0;
+
   final resolutionPresets = ResolutionPreset.values;
+
   ResolutionPreset currentResolutionPreset = ResolutionPreset.high;
 
   void onNewCameraSelected(CameraDescription cameraDescription) async {
@@ -39,6 +48,25 @@ class _CameraScreenState extends State<CameraScreen>
 
     try {
       await cameraController.initialize();
+      await Future.wait([
+        cameraController
+            .getMinExposureOffset()
+            .then((value) => _minAvailableExposureOffset = value),
+        cameraController
+            .getMaxExposureOffset()
+            .then((value) => _maxAvailableExposureOffset = value),
+        cameraController
+            .getMaxZoomLevel()
+            .then((value) => _maxAvailableZoom = value),
+        cameraController
+            .getMinZoomLevel()
+            .then((value) => _minAvailableZoom = value),
+      ]);
+
+      print('getMinExposureOffset: $_minAvailableExposureOffset');
+      print('getMaxExposureOffset: $_maxAvailableExposureOffset');
+      print('getMaxZoomLevel: $_maxAvailableZoom');
+      print('getMinZoomLevel: $_minAvailableZoom');
     } on CameraException catch (e) {
       print('Error initializing camera: $e');
     }
@@ -104,7 +132,7 @@ class _CameraScreenState extends State<CameraScreen>
                       padding: const EdgeInsets.all(8.0),
                       child: Container(
                         decoration: BoxDecoration(
-                          color: Colors.black38,
+                          color: Colors.black87,
                           borderRadius: BorderRadius.circular(10.0),
                         ),
                         child: Padding(
@@ -143,58 +171,100 @@ class _CameraScreenState extends State<CameraScreen>
                     alignment: Alignment.bottomCenter,
                     child: Padding(
                       padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
                         children: [
-                          InkWell(
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                Icon(
-                                  Icons.circle,
-                                  color: Colors.black38,
-                                  size: 60,
-                                ),
-                                Icon(
-                                  Icons.camera_rear,
-                                  // Icons.camera_front,
-                                  color: Colors.white,
-                                  size: 30,
-                                ),
-                              ],
-                            ),
-                          ),
-                          InkWell(
-                            child: Stack(
-                              alignment: Alignment.center,
-                              children: [
-                                Icon(
-                                  Icons.circle,
-                                  color: Colors.white38,
-                                  size: 80,
-                                ),
-                                Icon(
-                                  Icons.circle,
-                                  // Icons.camera_rear,
-                                  color: Colors.white,
-                                  size: 60,
-                                ),
-                              ],
-                            ),
-                          ),
-                          InkWell(
-                            child: Container(
-                              width: 60,
-                              height: 60,
-                              decoration: BoxDecoration(
-                                color: Colors.black,
-                                borderRadius: BorderRadius.circular(10.0),
-                                border: Border.all(
-                                  color: Colors.white,
-                                  width: 2,
+                          Row(
+                            children: [
+                              Expanded(
+                                child: Slider(
+                                  value: _currentZoomLevel,
+                                  min: _minAvailableZoom,
+                                  max: _maxAvailableZoom,
+                                  label: _currentZoomLevel.toString(),
+                                  activeColor: Colors.white,
+                                  inactiveColor: Colors.white30,
+                                  onChanged: (value) async {
+                                    setState(() {
+                                      _currentZoomLevel = value;
+                                    });
+                                    await controller!.setZoomLevel(value);
+                                  },
                                 ),
                               ),
-                            ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.black87,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text(
+                                      _currentZoomLevel.toStringAsFixed(1) +
+                                          'x',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              InkWell(
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.circle,
+                                      color: Colors.black38,
+                                      size: 60,
+                                    ),
+                                    Icon(
+                                      Icons.camera_rear,
+                                      // Icons.camera_front,
+                                      color: Colors.white,
+                                      size: 30,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              InkWell(
+                                child: Stack(
+                                  alignment: Alignment.center,
+                                  children: [
+                                    Icon(
+                                      Icons.circle,
+                                      color: Colors.white38,
+                                      size: 80,
+                                    ),
+                                    Icon(
+                                      Icons.circle,
+                                      // Icons.camera_rear,
+                                      color: Colors.white,
+                                      size: 60,
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              InkWell(
+                                child: Container(
+                                  width: 60,
+                                  height: 60,
+                                  decoration: BoxDecoration(
+                                    color: Colors.black,
+                                    borderRadius: BorderRadius.circular(10.0),
+                                    border: Border.all(
+                                      color: Colors.white,
+                                      width: 2,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
