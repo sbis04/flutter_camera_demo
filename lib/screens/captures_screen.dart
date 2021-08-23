56dc1303543673c87317aafb85ca334006ae3bf4
Souvik Biswas <sbis1999@gmail.com>
2021-08-23 08:09:15 +0530

    Update captures screen

--- lib/screens/captures_screen.dart
+++ lib/screens/captures_screen.dart
@@ -12,35 +12,55 @@ class CapturesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: [
-          for (File imageFile in imageFileList)
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 2,
-                ),
-              ),
-              child: InkWell(
-                onTap: () {
-                  Navigator.of(context).pushReplacement(
-                    MaterialPageRoute(
-                      builder: (context) => PreviewScreen(
-                        imageFileList: imageFileList,
-                        imageFile: imageFile,
-                      ),
-                    ),
-                  );
-                },
-                child: Image.file(
-                  imageFile,
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Captures',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  color: Colors.white,
                 ),
               ),
             ),
-        ],
+            GridView.count(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              crossAxisCount: 2,
+              children: [
+                for (File imageFile in imageFileList)
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 2,
+                      ),
+                    ),
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).pushReplacement(
+                          MaterialPageRoute(
+                            builder: (context) => PreviewScreen(
+                              imageFileList: imageFileList,
+                              imageFile: imageFile,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Image.file(
+                        imageFile,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
